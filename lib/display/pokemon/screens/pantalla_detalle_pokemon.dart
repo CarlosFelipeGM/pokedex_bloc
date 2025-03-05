057053756cb3e799a46750d87f5ce21d4d79f75b
Carlos Gomez <cgomezm@pucp.pe>
2025-03-04 22:39:32 -0500

    feat: link para detalle del pokemon y pantalla detalle

---
+++ lib/display/pokemon/screens/pantalla_detalle_pokemon.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_bloc/data/models/pokemon_model.dart';
+
+class PantallaDetallePokemon extends StatelessWidget {
+  final PokemonModel pokemon;
+
+  const PantallaDetallePokemon({required this.pokemon, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pokemon.name),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              pokemon.image,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/display/pokemon/ui/pokemon_tile.dart
+++ lib/display/pokemon/ui/pokemon_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_bloc/data/models/pokemon_model.dart';
+import 'package:pokedex_bloc/display/pokemon/screens/pantalla_detalle_pokemon.dart';
 
 class PokemonTile extends StatelessWidget {
   final PokemonModel pokemon;
@@ -13,6 +14,14 @@ class PokemonTile extends StatelessWidget {
       title: Text(pokemon.name.toUpperCase()),
       subtitle: Text(pokemon.url),
       trailing: const Icon(Icons.arrow_forward),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => PantallaDetallePokemon(pokemon: pokemon),
+          ),
+        );
+      },
     );
   }
 }
